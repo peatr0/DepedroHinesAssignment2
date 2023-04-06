42230f31292854dfaa56eefe9fe978d976f1f483
deped <depedro813@gmail.com>
2023-04-06 18:24:13 -0400

    9th commit. got some errors on the xml which took me some time to understand

--- app/src/main/java/depedro/hines/n01455125/MainActivity.java
+++ app/src/main/java/depedro/hines/n01455125/MainActivity.java
@@ -1,20 +1,78 @@
 package depedro.hines.n01455125;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DepedroHome depedroHome=new DepedroHome();
+    HinesDownload hinesDownload=new HinesDownload();
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.nav_view);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.Open_Nav,R.string.Close_Nav);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home:{
+                      //  getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, depedroHome).commit();
+                        return true;
+                    }
+                    case R.id.download:{
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,hinesDownload);
+                        return true;
+                    }
+
+
+                }
+               /* getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.framelayout, new DepedroHome())
+                        .commit();*/
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else super.onBackPressed();
+
+
     }
 }
