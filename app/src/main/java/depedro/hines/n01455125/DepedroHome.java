64323063b6cc04ce2e32a23f6d0e07c8f62596bd
deped <depedro813@gmail.com>
2023-04-06 00:24:54 -0400

    3rd commit built home fragment

---
+++ app/src/main/java/depedro/hines/n01455125/DepedroHome.java
@@ -0,0 +1,33 @@
+package depedro.hines.n01455125;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DepedroHome extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.depedro_home, container, false);
+
+        return view;
+    }
+}
