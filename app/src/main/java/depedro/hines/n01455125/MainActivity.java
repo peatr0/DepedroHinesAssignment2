3f9260a947d1edb7dcfd5e8ffc62c8caff14dd37
deped <depedro813@gmail.com>
2023-04-06 05:15:18 -0400

    8th commit.implemented the nav drawer

--- app/src/main/java/depedro/hines/n01455125/MainActivity.java
+++ app/src/main/java/depedro/hines/n01455125/MainActivity.java
@@ -1,11 +1,17 @@
 package depedro.hines.n01455125;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
