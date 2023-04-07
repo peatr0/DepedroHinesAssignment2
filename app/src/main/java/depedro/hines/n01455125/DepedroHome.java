551fd3b3c24270f0877f0734f4a0ff2cd384b428
deped <depedro813@gmail.com>
2023-04-06 23:51:05 -0400

    14th commit. generate random string and random number on home fragment

--- app/src/main/java/depedro/hines/n01455125/DepedroHome.java
+++ app/src/main/java/depedro/hines/n01455125/DepedroHome.java
@@ -3,16 +3,25 @@ package depedro.hines.n01455125;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import java.util.Random;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class DepedroHome extends Fragment {
-
-
-
+    TextView textView;
+    RadioGroup radioGroup;
+    RadioButton radioButton1,radioButton2,radioButton3;
+    Button button;
+    Random random;
+    int Random;
+    int minRandom=-50;
+    int maxRandom=50;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,23 @@ public class DepedroHome extends Fragment {
                              Bundle savedInstanceState) {
 
         View view= inflater.inflate(R.layout.depedro_home, container, false);
+        button=(view).findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r=new Random();
+                if (radioButton1.isChecked())
+                {
+
+                   Random = r.nextInt(maxRandom-minRandom+1)+minRandom;
+                   textView.setText(String.valueOf(random));
+                } else if (radioButton2.isChecked())
+                {
+                    String generatedString = RandomStringUtils.randomAlphabetic(10);
+
+                }
+            }
+        });
 
         return view;
     }
