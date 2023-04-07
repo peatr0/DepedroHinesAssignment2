bdf5028b47b7015b4c8c1eb615c94f8672f20135
deped <depedro813@gmail.com>
2023-04-07 02:49:34 -0400

    17th commit,working on download fragment now

--- app/src/main/java/depedro/hines/n01455125/HinesDownload.java
+++ app/src/main/java/depedro/hines/n01455125/HinesDownload.java
@@ -7,58 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HinesDownload#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HinesDownload extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HinesDownload() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HinesDownload.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HinesDownload newInstance(String param1, String param2) {
-        HinesDownload fragment = new HinesDownload();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView listView;
+    String[] list;  //= getResources().getStringArray(R.array.concepts);
+    String[] definition; // = getResources().getStringArray(R.array.Definitions);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        list = getResources().getStringArray(R.array.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,list);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.hines_download, container, false);
+
+        View view= inflater.inflate(R.layout.hines_download, container, false);
+
+        return view;
     }
 }
